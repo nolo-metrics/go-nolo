f409791363eafd99cffe7b8ff41372542618a5d2
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2015-02-08 23:08:08 -0800

    rework to use meter list

--- meter.go
+++ meter.go
@@ -6,6 +6,14 @@ type Metric struct {
 	Metadata   map[string]string
 }
 
+type Meter struct {
+	Identifier string
+	Metrics    []Metric
+}
+
+type MeterList []*Meter
+type MeterStringMap map[string][]map[string]string
+
 func (m Metric) ToMap() map[string]string {
 	result := make(map[string]string)
 	for k, v := range m.Metadata {
@@ -16,12 +24,7 @@ func (m Metric) ToMap() map[string]string {
 	return result
 }
 
-type Meter struct {
-	Identifier string
-	Metrics    []Metric
-}
-
-func (p Meter) ToMap() map[string][]map[string]string {
+func (p Meter) ToStringMap() MeterStringMap {
 	result := make(map[string][]map[string]string)
 	length := len(p.Metrics)
 	values := make([]map[string]string, length)
@@ -29,5 +32,18 @@ func (p Meter) ToMap() map[string][]map[string]string {
 		values[i] = m.ToMap()
 	}
 	result[p.Identifier] = values
+	return result
+}
+
+func (ml MeterList) ToStringMap() MeterStringMap {
+	result := make(map[string][]map[string]string)
+	for _, mr := range ml {
+		length := len(mr.Metrics)
+		values := make([]map[string]string, length)
+		for i, mc := range mr.Metrics {
+			values[i] = mc.ToMap()
+		}
+		result[mr.Identifier] = values
+	}
 	return result
 }
